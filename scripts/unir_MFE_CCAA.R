c316f379aaaa0822e46d56d152163c2e63d968b2
JRRaya <a02racaj@uco.es>
2025-10-07 23:01:39 +0200

    Configuración de guardado
    Se cambió el formato de salida del archivo:
    > st_write(MFE,
    +          dsn = "outputs/MFE.shp",
    +          delete_dsn = TRUE)
    Deleting source `outputs/MFE.shp' failed
    Writing layer `MFE' to data source `outputs/MFE.shp' using driver `ESRI Shapefile'
    Writing 1392101 features with 16 fields and geometry type Unknown (any).
    Failed to create feature 897482 in MFE
    Error: Feature creation failed.
    Además: Avisos:
    1: In CPL_write_ogr(obj, dsn, layer, driver, as.character(dataset_options),  :
      GDAL Error 1: outputs/MFE.shp does not appear to be a file or directory.
    2: In CPL_write_ogr(obj, dsn, layer, driver, as.character(dataset_options),  :
      GDAL Message 1: 2GB file size limit reached for outputs\MFE.shp. Going on, but might cause compatibility issues with third party software
    3: In CPL_write_ogr(obj, dsn, layer, driver, as.character(dataset_options),  :
      GDAL Error 1: Failed to write shape object. The maximum file size of 4294964736 has been reached. The current record of size 5336 cannot be added.

--- scripts/unir_MFE_CCAA.R
+++ scripts/unir_MFE_CCAA.R
@@ -4,8 +4,7 @@ rm(list = ls(all.names = TRUE))
 pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
 
 ## 1.2. Cargamos el paquete "sf"
-library(sf)
-library(dplyr)
+pacman::p_load(sf, dplyr)
 
 ## 1.3. Cargamos los datos de cada una de las CCAA
 ### 1.3.1. Definimos las rutas delas capas
@@ -20,8 +19,9 @@ rutas <- rutas[!grepl("\\.xml$", rutas)]
 ### 1.3.3. Definimos los nombres de las capas
 nombres <- basename(dirname(rutas))
 
-### 1.3.4. Creamos una lista donde guardar las capas cargadas
-lista <- list()
+### 1.3.4. Creamos una lista donde guardar las capas filtradas y otra en la que guardar las no filtradas
+lista_filtrada <- list()
+lista_no_filtrada <- list()
 
 ### 1.3.5. Establecemos el CRS objetivo a partir de la capa de madrid (comprobado en QGIS)
 m <- st_read(rutas[8])
@@ -30,22 +30,22 @@ rm(m)
 gc()
 
 ### 1.3.5. Definimos los nombres de las columnas a conservar
-nombres_columnas <- c(Superficie,
-                      FCCARB,
-                      FORARB,
-                      FormArbol,
-                      Distribuci,
-                      n_sp1,
-                      Especie1,
-                      O1,
-                      n_estado1,
-                      Estado1,
-                      FCCTOT,
-                      TIPOBOSQUE,
-                      UsoIFN,
-                      UsoMFE,
-                      RegBio,
-                      LULUCF)
+nombres_columnas <- c("Superficie",
+                      "FCCARB",
+                      "FORARB",
+                      "FormArbol",
+                      "Distribuci",
+                      "n_sp1",
+                      "Especie1",
+                      "O1",
+                      "n_estado1",
+                      "Estado1",
+                      "FCCTOT",
+                      "TIPOBOSQUE",
+                      "UsoIFN",
+                      "UsoMFE",
+                      "RegBio",
+                      "LULUCF")
 
 ### 1.3.6. Cargamos las capas en al lista
 for(ruta in rutas) {
@@ -70,40 +70,48 @@ for(ruta in rutas) {
     }
   }
   
-  # 3. Filtramos las columnas que quermos conservar
-  # Estas vienen recogidas en el archivo .xlsx (adjuntado a la información cartográfica descargada) 
-  capa <- capa %>% 
-    select(nombres_columnas)
+  # 4. Guardamos la capa sin filtrar en la lista de capas sin filtrar
+  lista_no_filtrada[[length(lista_no_filtrada) + 1]] <- capa
   
-  # 3. Comprobamos que la capa contenga inconsistencias
+  # 4. Filtramos las columnas que quermos conservar
+  # Estas vienen recogidas en el archivo .xlsx (adjuntado a la información cartográfica descargada) 
+  capa_filtrada <- capa %>% 
+    select(any_of(nombres_columnas))
+  
+  # 5. Comprobamos que la capa contenga inconsistencias
   # - True, se hace valida mediante st_make_valid()
-  if(any(!st_is_valid(capa))) {
-    capa <- st_make_valid(capa)
+  if(any(!st_is_valid(capa_filtrada))) {
+    capa <- st_make_valid(capa_filtrada)
   }
   
-  # 4. Guardamos la capa final en la lista de capas
-  lista[[length(lista) + 1]] <- capa
+  # 6. Guardamos la capa filtrada en la lista de capas filtradas
+  lista_filtrada[[length(lista_filtrada) + 1]] <- capa_filtrada
   
-  # 5. Liberamos la memoria RAM
+  # 7. Liberamos la memoria RAM
   rm(capa)
   gc()
 }
 
 ### 1.3.6. Nombramos las capas de la lista
-names(lista) <- nombres
+names(lista_filtrada) <- nombres
 
 # 2. Unimos los atributos de todas las capas en una sola capa vectorial
-MFE <- bind_rows(lista)
+MFE <- bind_rows(lista_filtrada)
 
-## 2.1. Eliminamos la lista, de modo que que liberemos la RAM
-rm(lista)
+# MFE <- MFE %>% 
+#   select(nombres_columnas)
+
+## 2.1. Eliminamos las listas, de modo que que liberemos la RAM
+rm(lista_no_filtrada)
+rm(lista_filtrada)
 gc()
 
-## 2.1. Comprobamos que no se hayan creado geometrias invalidas
-if(any(!st_is_valid(MFE))) {
-  MFE <- st_make_valid(MFE)
-}
+## 2.2. Comprobamos que no se hayan creado geometrias invalidas
+# if(any(!st_is_valid(MFE))) {
+#   MFE <- st_make_valid(MFE)
+# }
 
 # 3. Guardamos el resultado
 st_write(MFE,
-         dsn = "outputs/MFE.shp")
+         dsn = "outputs/MFE.gpkg",
+         delete_dsn = TRUE)
